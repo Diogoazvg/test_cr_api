02df8c12a5b3362f45b2dfbfec3bfe7919c162d3
Diogo Azvg <diogo.azvg@gmail.com>
2019-09-10 20:19:25 -0300

    Validate for stock non less 0

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -15,6 +15,7 @@ class Medicine < ApplicationRecord
 
   validates :value, presence: true
   validates :quantity, presence: true
+  validates :stock, numericality: { greater_than_or_equal_to: 0 }
   def total
     value * quantity
   end
